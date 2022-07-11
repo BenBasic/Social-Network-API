25c21ea268c5e12eb53f0e13239f035c8611a1a5
BenBasic <benjaminmbasic@gmail.com>
2022-07-11 19:41:23 -0400

    Added email property to the user model which includes a regex check for email validation, not entirely sure if it works quite yet but I will check for that with later testing

--- models/user.js
+++ models/user.js
@@ -13,5 +13,17 @@ const userSchema = new Schema({
             unique: true,
             trim: true
         },
+        /* Same as above but also using a match to validate the email address using a regex,
+        the regex is basically performing a check on the format and appropriate characters that would be in a standard email address,
+        first checking for characters a-z, 0-9, _.-, then @, then digit characters (0-9), a-z, .-, then . then a-z, .,
+        this would roughly translate into the typical email format of something@something.com
+        */
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
+        },
 
 })

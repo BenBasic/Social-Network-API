04b2da2bd389ead189a099158ccc267e53a86912
BenBasic <benjaminmbasic@gmail.com>
2022-07-12 19:34:37 -0400

    Added getUserById to user-controller.js, also added comments to the document, will continue adding more functionality to this controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,17 +2,45 @@
 const { User, Thought } = require('../models')
 
 const userController = {
+    /* Gets all the users from the userSchema, uses User.find to find all documents within it,
+    then populates the thoughts path while excluding the __v property
+    */
     getAllUsers(req, res) {
         User.find({})
         .populate({
             path: 'thoughts',
             select: ('-__v')
         })
-        .select('-__v')
-        .then(dbUserData => res.json(dbUserData))
+        .select('-__v') // Excludes the __v property
+        .then(dbUserData => res.json(dbUserData)) // Returning the result data as JSON Object
         .catch(err => {
+            // if there is an error, it will log an error
             console.log(err);
             res.status(400).json(err)
+        })
+    },
+    /* Gets one of the users from the userSchema, uses User.findOne to find one document within that has a matching id parameter,
+    then populates the thoughts path while excluding the __v property
+    */
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v') // Excludes the __v property
+        .then((dbUserData) => {
+            // If there is no matching id for the User requested, log an error
+            if(!dbUserData) {
+                res.status(404).json({ message: "No user with this id exists" });
+                return;
+            }
+            res.json(dbUserData); // Returning the result data as JSON Object
+        })
+        .catch((err) => {
+            // if there is an error, it will log an error
+            console.log(err);
+            res.status(400).json(err);
         })
     },
 }

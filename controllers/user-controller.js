a182b978164af865360a5d6731d9992bf469dd7d
BenBasic <benjaminmbasic@gmail.com>
2022-07-12 19:15:55 -0400

    Created index.js, created user-controller.js, added getAllUsers to the user-controller, working now to add more functionality to the controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,18 @@
+// Setting up file requirements
+const { User, Thought } = require('../models')
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: ('-__v')
+        })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        })
+    },
+}
---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Setting up file requirements
+const User = require('./User');
+const Thought = require('./Thought')
+
+// Exports modules for use in other files
+module.exports = {User, Thought};

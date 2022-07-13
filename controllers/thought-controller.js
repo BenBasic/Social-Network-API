7dd9a3d7a7e384dcfb4782fea9ac3d7c3d2337e5
BenBasic <benjaminmbasic@gmail.com>
2022-07-12 21:17:44 -0400

    Added getThoughtById functionality to the thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -16,5 +16,24 @@ const thoughtController = {
             res.status(400).json(err)
         })
     },
-    
+    /* Gets one of the thoughts from the thoughtSchema, uses Thought.findOne to find one document within that has a matching id parameter,
+    then excludes the __v property
+    */
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+        .select("-__v")
+        .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+                // If there is no matching id for the User requested, log an error
+                res.status(404).json({ message: "No thought with this id exists" });
+                return;
+            }
+            res.json(dbThoughtData); // Returning the result data as JSON Object
+        })
+        .catch((err) => {
+            // if there is an error, it will log an error
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
 }
